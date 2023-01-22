a7747683c47f7bfd68f7e6db64591f40ec594018
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-01-22 11:57:05 +0530

    Time: 8 ms (7.88%), Space: 7.7 MB (51.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,14 +3,15 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        int dp[n];
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int prev2=nums[0], curr;
+        int prev1=max(nums[0],nums[1]);
         
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+           curr=max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
