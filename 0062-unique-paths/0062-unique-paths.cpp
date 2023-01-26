e4180af2066c5afadc36958a9130412c32019168
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-01-26 12:49:06 +0530

    Time: 4 ms (25.47%), Space: 9.9 MB (31.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,8 +13,13 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(100,vector<int>(100,-1));
+        vector<vector<int>> dp(100,vector<int>(100,1));
         
-        return help(0,0,m,n,dp);
+       for(int i=m-2;i>=0;i--)
+       {
+           for(int j=n-2;j>=0;j--)
+           dp[i][j]=dp[i+1][j]+dp[i][j+1];
+       }
+        return dp[0][0];
     }
 };
