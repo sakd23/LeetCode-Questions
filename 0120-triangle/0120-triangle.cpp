d102611e16c109b6a68512fdeeafb80640f5aede
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-01-27 00:06:06 +0530

    Time: 3 ms (97.74%), Space: 8.6 MB (80.05%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,9 +14,23 @@ public:
         if(dp[row][col]!=-1)return dp[row][col];
         return dp[row][col]=tri[row][col]+min(help(tri,row+1,col,dp),help(tri,row+1,col+1,dp));
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(300,vector<int>(300,-1));
-        
-        return help(triangle,0,0,dp);
+    int minimumTotal(vector<vector<int>>& tri) {
+        vector<int> dp(300),curr(300);
+        int m=tri.size();
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==m-1)curr[j]=tri[i][j];
+                else 
+                {
+                    curr[j]=tri[i][j]+min(dp[j],dp[j+1]);
+                   
+                }
+            }
+            
+             dp=curr;
+        }
+        return dp[0];
     }
 };
