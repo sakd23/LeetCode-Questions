0622387e089c9d5398040231fb1338341c0a6586
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-01-27 11:45:36 +0530

    Time: 2773 ms (5.03%), Space: 426.9 MB (5.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -27,8 +27,46 @@ int sign[3]={0,-1,+1};
         
     }
     int cherryPickup(vector<vector<int>>& grid) {
+        
         vector<vector<vector<int>>> dp(200,vector<vector<int>>(200,vector<int>(200,-1)));
-        int row=grid.size(),col=grid[0].size();
-        return help(grid,0,0,0,col-1,row,col,dp);
+                int row=grid.size(),col=grid[0].size();
+
+        for(int i=0;i<col;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(i==j)dp[row-1][i][j]=grid[row-1][i];
+                else dp[row-1][i][j]=grid[row-1][i]+grid[row-1][j];
+            }
+        }
+        
+        for(int i=row-2;i>=0;i--)
+        {
+            for(int j=0;j<col;j++)
+            {
+                for(int k=0;k<col;k++)
+                {
+                    if(j==k)dp[i][j][k]=grid[i][j];
+                    else dp[i][j][k]=grid[i][j]+grid[i][k];
+                    int ans=-1e8;
+                    for(int l=-1;l<2;l++)
+                    {
+                        for(int m=-1;m<2;m++)
+                        {
+                            int one=0;
+                            if(j+l>-1 and j+l<col and k+m>-1 and k+m<col)
+                            {
+                                one=dp[i+1][k+m][j+l];
+                            }
+                            
+                            
+                            ans=max(ans,one);
+                        }
+                    }
+                    dp[i][j][k]+=ans;
+                }
+            }
+        }
+        return dp[0][0][col-1];
     }
 };
