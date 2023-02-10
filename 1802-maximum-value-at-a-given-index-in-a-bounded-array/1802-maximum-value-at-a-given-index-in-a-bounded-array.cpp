97d4aa09846bf06a1b8d647f2069ce6ff5a43cad
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-02-11 00:34:54 +0530

    Time: 4 ms (44.51%), Space: 5.9 MB (89.66%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int maxValue(int n, int index, int msum) {
-        
+       
         long long left=(long long)1,right=(long long)msum,mid,ans=(long long)1;
         
         while(left<=right)
         {
             mid=left+(right-left)/2;
            
-            long long leftover=mid-(index+1),leftover2=mid-(n-index),total=0;
+            long long a=max(1ll,mid-index),b=mid,a1=max(1ll,mid-(n-1-index)),b1=mid,total=0;
             
-             if(mid-(index+1)<=0)
-            {
-                total=(index+1)-mid;
-                leftover=0;
-            }
-            if(mid-(n-index)<=0)
-            {
-              total=total-mid+(n-index);  
-                leftover2=0;
-            }
-            long long sum=(mid*(mid+1))-((leftover*(leftover+1))/2)-mid-((leftover2*(leftover2+1))/2)+total;
+             if(mid<=(index+1))total=(index+1)-mid;
+            if(mid<=((n)-index))total=total-(mid-((n)-index));
+            long long sum=((((a+b)*(b-a+1))/2)+(((a1+b1)*(b1-a1+1))/2))-mid+(total);
            
             if(sum<=(long long)msum)
             {
