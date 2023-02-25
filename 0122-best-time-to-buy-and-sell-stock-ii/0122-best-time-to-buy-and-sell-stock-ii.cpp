1f73d17fe1fbddbdbd70d459bf6d3b7912122e7d
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-02-26 00:10:15 +0530

    Time: 18 ms (13.57%), Space: 17.3 MB (7.46%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
+    int help(vector<int>& a,vector<vector<int>>& dp,int i,int st)
+    {
+        if(i>=a.size())return 0;
+        if(dp[i][st]!=-1)return dp[i][st];
+        int ans;
+        if(st==0)ans=max(help(a,dp,i+1,1)-a[i],help(a,dp,i+1,st));
+        else ans=max(help(a,dp,i+1,0)+a[i],help(a,dp,i+1,st));
+        return dp[i][st]=ans;
+    }
     int maxProfit(vector<int>& a) {
         int n=a.size();
-        int dp[n+1][2];
-      memset(dp,0,sizeof(dp));
+        // int dp[n+1][2];
+        vector<vector<int>>  dp(n+1,vector<int>(2,-1));
+//       memset(dp,0,sizeof(dp));
         
-        for(int i=n-1;i>=0;i--)
-        {
-            dp[i][1]=max(a[i]+dp[i+1][0],dp[i+1][1]);
-            dp[i][0]=max(dp[i+1][1]-a[i],dp[i+1][0]);
-        }
-        return dp[0][0];
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             dp[i][1]=max(a[i]+dp[i+1][0],dp[i+1][1]);
+//             dp[i][0]=max(dp[i+1][1]-a[i],dp[i+1][0]);
+//         }
+        return help(a,dp,0,0);
     }
 };
