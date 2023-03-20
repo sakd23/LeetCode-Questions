60c37bea0c9f5c96a7f21031b1886cb501a96b09
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-03-20 15:05:43 +0530

    Time: 235 ms (9.58%), Space: 49.3 MB (15.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,6 +3,7 @@ public:
     
     int help(vector<int>& coins,int amt,int idx,vector<vector<int>>& dp)
     {
+        
         if(amt==0)
         {
             return 0;
@@ -20,9 +21,31 @@ public:
     }
     
     int coinChange(vector<int>& coins, int amt) {
+        int n=coins.size();
         vector<vector<int>> dp(coins.size()+1,vector<int>(amt+1,-1));
-        int ans=help(coins,amt,0,dp);
-        if(ans>=1e5)return -1;
-        return ans;
+        for(int i=n;i>=0;i--)
+        {
+            for(int j=0;j<=amt;j++)
+            {
+                if(i==n)
+                {
+                    dp[i][j]=1e5;
+                    continue;
+                }
+                
+                if(j==0)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                
+                dp[i][j]=dp[i+1][j];
+                if(j>=coins[i])dp[i][j]=min(dp[i][j],1+dp[i][j-coins[i]]);
+                
+            }
+        }
+        // int ans=help(coins,amt,0,dp);
+        // if(ans>=1e5)return -1;
+        return dp[0][amt]>=1e5?-1:dp[0][amt];
     }
 };
