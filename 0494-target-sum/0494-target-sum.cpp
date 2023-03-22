b6793eb6e1d121dcfc0b8a0a34c92e6ed8272646
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-03-22 14:56:46 +0530

    Time: 52 ms (48.41%), Space: 16.6 MB (28.83%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -15,10 +15,18 @@ public:
                 if(j>=a[i])dp[i][j]=dp[i][j]+dp[i+1][j-a[i]];
             }
         }
-        for(int i=0;i<=tot/2;i++)
-        {
-            if(abs(i-(tot-i))==abs(tar))ans+=dp[0][i];
-        }
-        return ans;
+        
+        int tem=(tot-tar)/2;
+        if(abs(tar)>tot or (tot-tar)%2)return 0;
+        return dp[0][tem];
+        
+//         for(int i=0;i<=tot/2;i++)
+//         {
+//             // abs of target for negtive targets.
+//             //what is true for 10 is also true for -10.
+//             //{15},{4,1} gives both diff of 10 and -10.
+//             if((tot-i)-i)==abs(tar))ans+=dp[0][i];
+//         }
+//         return ans;
     }
 };
