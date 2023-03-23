1f905d08ca90b5267e418f84dee4a6a536d549cf
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-03-23 11:37:33 +0530

    Time: 8 ms (92.99%), Space: 7.2 MB (83.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& a) {
+    int change(int amt, vector<int>& a) {
         int n=a.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
-        for(int i=0;i<=amount;i++)
-        {
-            dp[n][i]=0;
-        }
+       vector<int> dp(amt+1,0);
+        dp[0]=1;
         
-         for(int i=0;i<=n;i++)
+        for(auto x:a)
         {
-            dp[i][0]=1;
-        }
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=1;j<=amount;j++)
+            for(int  i=x;i<=amt;i++)
             {
-                dp[i][j]=dp[i+1][j];
-                if(j>=a[i])dp[i][j]+=dp[i][j-a[i]];
+                dp[i]+=dp[i-x];
             }
         }
-        return dp[0][amount];
-        
+        return dp[amt];
     }
 };
