9cc56d5b26e3bde649d11e8876719fdf34c8f357
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-03-28 23:59:54 +0530

    Time: 4 ms (69.83%), Space: 9.6 MB (57.33%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-       int dp[2000];
-        memset(dp,0,sizeof(dp));
-        int last=days.back();
-      int  curr=days.size()-1;
-        // dp[last]=min({costs[0],costs[1],costs[2]});
-       for(int i=last;i>=1;i--)
-       {
-           if(i==days[curr])
-           {
-                dp[i]=costs[0]+dp[i+1];
-           dp[i]=min(dp[i],costs[1]+dp[i+7]);
-           dp[i]=min(dp[i],costs[2]+dp[i+30]);
-              // cout<<i<<" "<<dp[i]<<endl;
-               curr--;
-               if(curr<0)break;
-               continue;
-           }
-           else
-           dp[i]=dp[i+1];
-
-       }
-        return dp[days[0]];
+        vector<int> dp(500,0);
+        int n=days.size(),idx=n-1;
+        for(int i=365;i>=1;i--)
+        {
+            
+            
+            if(idx>=0 and days[idx]==i)
+            {
+               dp[i]=min({costs[0]+dp[i+1],costs[1]+dp[i+7],costs[2]+dp[i+30]});
+             idx--;
+            }
+            else dp[i]=dp[i+1];
+        }
+        return dp[1];
     }
 };
