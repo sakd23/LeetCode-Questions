aed4e2c90861ca3951205fad8d1069cd5ffda40a
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-04-03 18:13:24 +0530

    Time: 474 ms (32.73%), Space: 276.7 MB (6.27%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool pal(string& str,int s,int e)
+    {
+        while(s<e)
+        {
+            if(str[s]!=str[e])return false;
+            s++;
+            e--;
+        }
+        return true;
+    }
+    int help(string s,int srt,int n,int dp[])
+    {
+          if(srt>=n)return 0;
+        if(pal(s,srt,n))return 0;
+      
+        if(dp[srt]!=-1)return dp[srt];
+        int fans=INT_MAX;
+        for(int i=srt;i<n;i++)
+        {
+            if(pal(s,srt,i))
+            {
+            int ans=1+help(s,i+1,n,dp);
+            fans=min(fans,ans);
+            }
+            
+        }
+        return dp[srt]=fans;
+    }
+    int minCut(string s) {
+        int n=s.size();
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        return help(s,0,n-1,dp);
+    }
+};
