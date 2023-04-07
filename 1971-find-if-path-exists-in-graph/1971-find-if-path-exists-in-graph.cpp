1b9ff47c290538d0a2e582ca3c74bfaa9020d983
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-04-07 23:45:47 +0530

    Time: 737 ms (48.52%), Space: 174.4 MB (57.14%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -16,13 +16,38 @@ public:
         return ans;
     }
     bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
+        if(s==d)return true;
         vector<vector<int>> a(n);
-        vector<int> vis(n+1,0);
+      
         for(auto x:edges)
         {
             a[x[0]].push_back(x[1]);
             a[x[1]].push_back(x[0]);
         }
-        return dfs(a,n,s,d,vis);
+       queue<int> q;
+        vector<int> vis(n,0);
+        q.push(s);
+       vis[s]=1;
+        
+        while(!q.empty())
+        {
+            int siz=q.size();
+            for(int i=0;i<siz;i++)
+            {
+                int temp=q.front();
+                if(temp==d)return true;
+               
+                q.pop();
+                for(auto x:a[temp])
+                {
+                    if(!vis[x])
+                    {
+                        q.push(x);
+                        vis[x]=1;
+                    }
+                }
+            }
+        }
+        return false;
     }
 };
