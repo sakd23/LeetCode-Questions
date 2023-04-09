c9836c1609ef8260901abe08264bfda4c26b74ae
sakd23 <89689939+sakd23@users.noreply.github.com>
2023-04-09 17:53:44 +0530

    Time: 11 ms (38.17%), Space: 13.3 MB (30.66%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& a) {
         int m=a.size(),n=a[0].size();
-        vector<vector<int>> ans(m,vector<int>(n,INT_MAX)),vis(m,vector<int>(n,0));
-   
+           int fans=0;
+        vector<vector<int>> vis(m,vector<int>(n,0));
+    queue<pair<int,int>> q;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(a[i][j]==2)
-                {
-                     queue<pair<int,int>> q;
-                   
-                    ans[i][j]=0;
-                    q.push({i,j});
-                    vis[i][j]=1;
-                    while(!q.empty())
+                                if(a[i][j]==2)
+                                  {
+                                    q.push({i,j});
+                                    vis[i][j]=1;
+                                  }
+            }
+        }
+        
+        while(!q.empty())
                     {
-                        pair<int,int> p=q.front();
+                        int siz=q.size();
+            while(siz--)
+            {
+                pair<int,int> p=q.front();
                         q.pop();
                         int cr=p.first;
                         int cc=p.second;
-                         int curr=ans[cr][cc];
-                        // cout<<curr<<endl;
+                       
+                       
                         for(int i=-1;i<=1;i++)
                         {
                             for(int j=-1;j<=1;j++)
@@ -32,7 +37,7 @@ public:
                                   if(cr+i>=0 and cr+i<m and cc+j<n and cc+j>=0 and a[cr+i][cc+j]==1 and !vis[cr+i][cc+j])
                                   {
                                       
-                                      ans[cr+i][cc+j]=min(curr+1,ans[cr+i][cc+j]);
+                                      
                                       vis[cr+i][cc+j]=1;
                                       q.push({cr+i,cc+j});
                                       
@@ -40,28 +45,19 @@ public:
                                 }
                             }
                         }
-                    }
-                    
-                    for(int i=0;i<m;i++)
-                    {
-                        for(int j=0;j<n;j++)
-                        {
-                            vis[i][j]=0;
-                        }
-                    }
-                }
+                
             }
-        }
-        int fans=0;
+            if(!q.empty())
+            fans++;
+                     } 
+     
                        for(int i=0;i<m;i++)
                     {
                         for(int j=0;j<n;j++)
                         {
-                            if(a[i][j]==1)
+                            if(a[i][j]==1 and !vis[i][j])
                             {
-                                if(ans[i][j]==INT_MAX)return -1;
-                                
-                                fans=max(fans,ans[i][j]);
+                                return -1;
                             }
                         }
                     }
